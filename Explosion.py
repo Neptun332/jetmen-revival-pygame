4a016f9a2cfb22cc53bdf521df2722e1e9eeb816
Neptun332 <patrykbednarz8@gmail.com>
2022-11-11 12:23:58 +0100

    refactored explosion added comments

--- Explosion.py
+++ Explosion.py
@@ -10,11 +10,16 @@ class Explosion:
         self.world = world
 
     def trigger(self):
+        # getting left corner of square, a = 2 * radius
         start_x = self.center_x - self.radius
         start_y = self.center_y - self.radius
+        # going for all pixels in that square
         for x in range(start_x, self.center_x + self.radius):
             for y in range(start_y, self.center_y + self.radius):
-                # equation of circle: (x - a)^2 + (y - b)^2 = r^2
-                if ((x - self.center_x) * (x - self.center_x)
-                        + (y - self.center_y) * (y - self.center_y)) <= self.radius * self.radius:
+                if self._is_position_inside_circle(x, y):
                     self.world.remove_tile(x, y)
+
+    def _is_position_inside_circle(self, x: int, y: int):
+        # equation of circle: (x - a)^2 + (y - b)^2 = r^2
+        return ((x - self.center_x) * (x - self.center_x) + (y - self.center_y) * (
+                y - self.center_y)) < self.radius * self.radius
